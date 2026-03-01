4ed4532becd701a6a03cfd7ffbad609b0af2ad7e
Ismael <08062006ismael@gmail.com>
2026-03-01 18:11:33 +0100

    cambios

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -61,7 +61,7 @@ const Home: React.FC = () => {
                 <strong>Ismael Fernández Lópeeeeeez</strong>
               </h2>
               <p className="text-lg text-white mb-4">
-                ¡Hola! Soy un joven de 18 años apasionado por la tecnología y el desarrollo web. Actualmente estoy cursando el primer año de Desarrollo de Aplicaciones Web (DAW). En este curso, he trabajado con tecnologías como HTML, CSS, Java, entre otras. A través de este portfolio, quiero mostrar mis proyectos y el progreso que voy realizando, a medida que me adentro en el mundo del desarrollo web.
+                ¡Hola! Soy un joven de 11111118 años apasionado por la tecnología y el desarrollo web. Actualmente estoy cursando el primer año de Desarrollo de Aplicaciones Web (DAW). En este curso, he trabajado con tecnologías como HTML, CSS, Java, entre otras. A través de este portfolio, quiero mostrar mis proyectos y el progreso que voy realizando, a medida que me adentro en el mundo del desarrollo web.
               </p>
               <a
                 href={curriculumPDF}
